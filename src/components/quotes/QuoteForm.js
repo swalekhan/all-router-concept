171a85050898da14dfaccd6d4af768982aaa461f
swalekhan <swalekhan1522@gmail.com>
2023-01-02 13:18:44 +0530

    focus-promt-and-sortin-machensim

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,11 @@
-import { useRef } from 'react';
-
+import { useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isEntring, setIsEntring] = useState(false)
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -18,10 +19,20 @@ const QuoteForm = (props) => {
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
+  
+  const clickHandler = ()=>{
+    setIsEntring(false)
+  }
+
+  const focusHandler = () =>{
+   setIsEntring(true)
+  }
 
   return (
+    <>
+    <Prompt when={isEntring} message ={(e)=>"are you sure to leave! your data will be lost"}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={focusHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +48,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={clickHandler} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </>
   );
 };
 
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,38 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) =>{
+  quotes.sort((quoteA,quoteB)=>{
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  })
+}
+
 const QuoteList = (props) => {
+  const history = useHistory()
+  const location = useLocation()
+
+  const queryParam = new URLSearchParams(location.search)   // javascript constructor
+  const isSortingAscending = queryParam.get('sort') === "asc"   // we can call this function inside sortHandler func;
+ 
+  sortQuotes(props.quotes, isSortingAscending) 
+
+  const sortingHandler = () => {
+    history.push('/quotes?sort='+(isSortingAscending?"desc":"asc")) //history render page whenever it changes url, quotes?sort this is a optional chaining; if the value exist after ? sign ia present then change page or stay on quotes; 
+  }
+
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortingHandler}>Sort {isSortingAscending?"Descending":"Ascending"}</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
